2fc2e4aa297b244d762782fd8a84e5847ba04765
Zakhor <49256667+zakhor@users.noreply.github.com>
2020-11-01 19:56:25 +0900

    Fix encode

--- hosh_v3.1.1.py
+++ hosh_v3.1.1.py
@@ -110,10 +110,10 @@ def hosh(key, time_interval, target, name, message):
 def search_thread(section, time_interval, target, name, message):
   while 1:
     response = requests.get('https://' + domain + '.5ch.net/' + bbs + '/subback.html') #スレ一覧を開く
-    response.encoding = response.apparent_encoding 
+    response.encoding = 'Shift_JIS'
     pattern_find_thread = '<a href=\"([0-9]{10})/l50\">.+?' + target + '.+?</a>'
     key = re.search(pattern_find_thread, response.text) #スレを検索
-    if key: #スレが見つかったら保守
+  if key: #スレが見つかったら保守
       url_thread = 'https://' + domain + '.5ch.net/test/read.cgi/' + bbs + '/' + key.groups()[0] + '/l0'
       print_lock('THREAD FOUND\n'
       f'url_thread:\t{url_thread}\n', lock) 
